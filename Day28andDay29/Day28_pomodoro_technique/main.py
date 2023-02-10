4605f2535c42caf688cb83c5e1c840946b9999b1
ManasGupta <manas.gupta@nexturn.com>
2023-02-10 12:25:04 +0530

    Day28

--- Day28andDay29/Day28_pomodoro_technique/main.py
+++ Day28andDay29/Day28_pomodoro_technique/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,26 +10,64 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+time = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset():
+    window.after_cancel(time)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer.config(text="Timer")
+    checkbox.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5*60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    # If it's the 1st/3rd/5th/7th rep:
+    if reps % 2 != 0:
+        count_down(work_sec)
+        timer.config(text="Work", fg=GREEN)
+
+    elif reps % 8 == 0:
+        # If it's the 8th rep:
+        count_down(long_break_sec)
+        timer.config(text="Break", fg=RED)
+    else:
+        # if it's 2nd/4th/6th rep:
+        count_down(short_break_sec)
+        timer.config(text="Break", fg=PINK)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
+
     if count_min < 10:
-        count_min = "0"+str(count_min)
+        count_min = "0" + str(count_min)
     if count_sec < 10:
         count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global time
+        time = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        for _ in range(math.floor(reps / 2)):
+            mark += "✔"
+        checkbox.config(text=mark)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -48,10 +87,10 @@ canvas.grid(row=1, column=1)
 start_button = Button(text="Start", bg="white", highlightthickness=0, command=start_timer)
 start_button.grid(row=2, column=0)
 
-reset_button = Button(text="Reset", bg="white", highlightthickness=0)
+reset_button = Button(text="Reset", bg="white", highlightthickness=0, command=reset)
 reset_button.grid(row=2, column=2)
 
-checkbox = Label(text="🗸", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
+checkbox = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
 checkbox.grid(row=3, column=1)
 
 window.mainloop()
